f6e811f566e17a94a311a46b7fb1c7a5d938ae06
Merisa <bishwakarma.merisa@gmail.com>
2022-04-14 12:01:12 +0100

    added Rest controller

--- src/main/java/com/qa/hobbyproject/web/RecipeController.java
+++ src/main/java/com/qa/hobbyproject/web/RecipeController.java
@@ -1,5 +1,17 @@
 package com.qa.hobbyproject.web;
 
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.qa.hobbyproject.domain.Recipe;
+
+@RestController 
 public class RecipeController {
+	
+	@PostMapping("/create")
+	public Recipe addRecipe(Recipe recipe) {
+		
+	}
+
 
 }
