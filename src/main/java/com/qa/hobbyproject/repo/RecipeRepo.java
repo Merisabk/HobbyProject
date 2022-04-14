c5053151458f544cb9eca46ac424d91c21bd8100
Merisa <bishwakarma.merisa@gmail.com>
2022-04-14 12:29:12 +0100

    added Repository

--- src/main/java/com/qa/hobbyproject/repo/RecipeRepo.java
+++ src/main/java/com/qa/hobbyproject/repo/RecipeRepo.java
@@ -1,5 +1,14 @@
 package com.qa.hobbyproject.repo;
 
-public class RecipeRepo {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-}
+import com.qa.hobbyproject.domain.Recipe;
+
+
+	@Repository
+	public interface RecipeRepo extends JpaRepository<Recipe, Integer> {
+		//Spring will auto-generate the basic CRUD functionality.
+	}
+	
+
