875fa1c8a17c871a2432fcb453bfab756e5262ce
Merisa <bishwakarma.merisa@gmail.com>
2022-04-14 13:39:26 +0100

    HP-27 #added CRUD,getbyName and getbyCategory methods

--- src/main/java/com/qa/hobbyproject/web/RecipeController.java
+++ src/main/java/com/qa/hobbyproject/web/RecipeController.java
@@ -5,12 +5,14 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.qa.demo.domain.Dog;
 import com.qa.hobbyproject.domain.Recipe;
 import com.qa.hobbyproject.service.RecipeService;
 
@@ -29,7 +31,7 @@ public class RecipeController {
 	@PostMapping("/create")
 	public ResponseEntity<Recipe> addRecipe(@RequestBody Recipe recipe) {
 		Recipe added = this.service.create(recipe);
-		ResponseEntity<Recipe> response = new ResponseEntity<Recipe> (created, HttpStatus.CREATED);
+		ResponseEntity<Recipe> response = new ResponseEntity<Recipe> (added, HttpStatus.CREATED);
 		return response;
 	}
 	
@@ -37,6 +39,40 @@ public class RecipeController {
 	@GetMapping("/getAll") 
 	public ResponseEntity<List<Recipe>> getAllListOfRecipes() {
 		return ResponseEntity.ok(this.service.getAll());
+	}
+	
+	// READ ONE
+	@GetMapping("/get/{id}")
+	public Recipe getRecipe(@PathVariable Integer id){
+		return this.service.getOne(id);
+	}
+	
+	//Update 
+	@PutMapping("/update/{id}")
+	public ResponseEntity<Recipe> updateRecipe(@PathVariable Integer id, @RequestBody Recipe newRecipe){
+		Recipe body = this.service.update(id, newRecipe);
+		ResponseEntity<Recipe> response = new ResponseEntity<Recipe>(body, HttpStatus.ACCEPTED);
+		return response;
+	}
+	//Delete 
+	@DeleteMapping("/delete/{id}") 
+	public ResponseEntity<?> deleteRecipe(@PathVariable Integer id) {
+		this.service.remove(id);
+		return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+	}
+	
+	//Read by name 
+	@GetMapping("/getByName/{name}")
+	public ResponseEntity<List<Recipe>> getRecipeByName(@PathVariable String name){
+	List<Recipe> found = this.service.getRecipeByName(name);
+	return ResponseEntity.ok(found);
+	}
+	
+	//read by category 
+	@GetMapping("/getByCategory/{category}")
+	public ResponseEntity<List<Recipe>> getRecipesByCategory(@PathVariable String category) {
+		List<Recipe> found = this.service.getRecipeByCategory(category);
+		return ResponseEntity.ok(found);
 	}
 
 }
