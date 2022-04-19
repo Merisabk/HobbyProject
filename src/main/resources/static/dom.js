8f1a0ade8582a82d495755dfaa787ccf1e25e98d
Merisa <bishwakarma.merisa@gmail.com>
2022-04-19 10:21:19 +0100

    HP-41 #added dom

---
+++ src/main/resources/static/dom.js
@@ -0,0 +1,17 @@
+export const inputRecipeName = document.getElementById(`inputRecipeName`);
+export const inputCategory = document.getElementById(`inputCategory`);
+export const inputServings = document.getElementById(`inputServings`);
+export const inputCookingTime = document.getElementById(`inputCookingTime`);
+export const inputIngredients = document.getElementById(`inputIngredients`);
+export const buttonUploadImage = document.getElementById(`buttonUploadImage`);
+export const buttonAddRecipe = document.getElementById(`buttonAddRecipe`);
+export const buttonEditRecipe = document.getElementById(`buttonEditRecipe`)
+export const buttonDeleteRecipe = document.getElementById(`buttonDeleteRecipe`)
+
+
+export const buttonSweet = document.getElementById(`buttonSweet`);
+export const buttonSavoury = document.getElementById(`buttonSavoury`);
+
+export const listSweetRecipes  = document.getElementById(`listSweetRecipes`);
+export const listSavouryRecipes  = document.getElementById(`listSavouryRecipes`);
+export const listAllRecipes  = document.getElementById(`listAllRecipes`);
